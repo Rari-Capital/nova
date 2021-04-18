d5fd45028b525c180978e10878eeac8610be4f4d
t11s <greenfeatherhelp@gmail.com>
2021-04-18 11:47:25 -0700

    🚧More WIP commits to L1_NovaExecutionManager

--- contracts/L1_NovaExecutionManager.sol
+++ contracts/L1_NovaExecutionManager.sol
@@ -19,8 +19,19 @@ interface iL2_NovaRegistry {
 }
 
 contract L1_NovaExecutionManager is OVM_CrossDomainEnabled {
-    bytes32 immutable HARD_REVERT_HASH = keccak256("__NOVA__HARD__REVERT__");
-    address immutable L2_NovaRegistry;
+    using SafeERC20 for IERC20;
+
+    /// @dev The hash of the hard revert message.
+    bytes32 private immutable HARD_REVERT_HASH =
+        keccak256("__NOVA__HARD__REVERT__");
+
+    /// @dev The address of the L2_NovaRegistry to send cross domain messages to.
+    address private immutable L2_NovaRegistry;
+
+    /// @dev The address of the strategy that is currenlty being called.
+    address private currentlyExecutingStrategy;
+    /// @dev The address who called `exec`/`execWithRecipient`.
+    address private currentExecutor;
 
     constructor(address _L2_NovaRegistry, address _l1messenger)
         OVM_CrossDomainEnabled(_l1messenger)
@@ -37,23 +48,21 @@ contract L1_NovaExecutionManager is OVM_CrossDomainEnabled {
     ) public {
         uint256 startGas = gasleft();
 
+        // Initialize execution context.
+        currentExecutor = msg.sender;
+        currentlyExecutingStrategy = strategy;
+
         // Call the strategy.
-        (bool success, bytes memory _returnData) = strategy.call(l1calldata);
+        (bool success, bytes memory returnData) = strategy.call(l1calldata);
 
-        // TODO: CAN WE FURTHER OPTIMIZE SINCE WE KNOW HOW LONG WE WANT THE REVERT STRING TO BE?
-        // If the call reverted, check if it's a hard revert:
-        if (!success && _returnData.length >= 68) {
-            // Isolate the revert message.
-            assembly {
-                _returnData := add(_returnData, 0x04)
-            }
+        // Revert if the strategy hard reverted.
+        require(!success && isHardRevert(returnData), "HARD_REVERT");
 
-            // TODO: WILL THIS WORK? (_returnData is raw bytes not decoded)
-            // Revert if this is a hard revert.
-            require(keccak256(_returnData) != HARD_REVERT_HASH, "HARD_REVERT");
-        }
+        // Reset execution context.
+        currentlyExecutingStrategy = address(0);
+        currentExecutor = address(0);
 
-        // Compute the execHash
+        // Compute the execHash.
         bytes32 execHash =
             keccak256(
                 abi.encodePacked(nonce, strategy, l1calldata, tx.gasprice)
@@ -66,8 +75,7 @@ contract L1_NovaExecutionManager is OVM_CrossDomainEnabled {
                 (xDomainMessageGasLimit / 32) +
                 74000;
 
-        // Figure out how much gas this call will take up in total.
-        // (Constant function call gas) + (Gas diff after calls) + (the amount of gas that will be burned via enqueue + storage/other message overhead)
+        // Figure out how much gas this call will take up in total: (Constant function call gas) + (Gas diff after calls) + (the amount of gas that will be burned via enqueue + storage/other message overhead)
         uint256 gasUsed = 21396 + (startGas - gasleft()) + xDomainMessageGas;
 
         // Send message to unlock the bounty on L2.
@@ -105,6 +113,28 @@ contract L1_NovaExecutionManager is OVM_CrossDomainEnabled {
     }
 
     function transferFromBot(address token, uint256 amount) external {
-        // TODO
+        // Only the currently executing strategy is allowed to call this method.
+        require(
+            msg.sender == currentlyExecutingStrategy,
+            "NOT_CURRENTLY_EXECUTING"
+        );
+
+        // Transfer the token from the calling bot the currently executing strategy (msg.sender is enforced to be the currentlyExecutingStrategy above).
+        IERC20(token).safeTransferFrom(currentExecutor, msg.sender, amount);
+    }
+
+    function isHardRevert(bytes memory returnData) private view returns (bool) {
+        // TODO: CAN WE FURTHER OPTIMIZE SINCE WE KNOW HOW LONG WE WANT THE REVERT STRING TO BE?
+        if (returnData.length >= 68) {
+            // Isolate the revert message.
+            assembly {
+                returnData := add(returnData, 0x04)
+            }
+
+            // TODO: WILL THIS WORK? (returnData is raw bytes not decoded)
+            return keccak256(returnData) != HARD_REVERT_HASH;
+        } else {
+            return false;
+        }
     }
 }
