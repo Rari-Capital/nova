fc41daaad92cf3d7e667a49de7e2d6635e39439f
t11s <greenfeatherhelp@gmail.com>
2021-04-25 14:59:35 -0700

    👷‍♂️ Don't export OVM typedefs

--- tasks/index.ts
+++ tasks/index.ts
@@ -3,8 +3,10 @@ import { task } from "hardhat/config";
 import { TASK_COMPILE } from "hardhat/builtin-tasks/task-names";
 
 task(TASK_COMPILE).setAction(async (args, hre: any, runSuper) => {
-  if (hre.network.config.ovm && !hre.config.typechain.outDir.endsWith("-ovm")) {
-    hre.config.typechain.outDir += "-ovm";
+  // Don't run typechain for OVM because they will get included with the L1 types.
+  if (hre.network.config.ovm) {
+    return runSuper({ ...args, noTypechain: true });
+  } else {
+    return runSuper(args);
   }
-  return runSuper(args);
 });
