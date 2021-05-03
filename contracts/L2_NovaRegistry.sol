a021dad7998e5768cc3bbae59c897fde463d7bdb
t11s <greenfeatherhelp@gmail.com>
2021-05-02 21:27:51 -0700

    ♻️ Remove console.sol

--- contracts/L2_NovaRegistry.sol
+++ contracts/L2_NovaRegistry.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@eth-optimism/contracts/libraries/bridge/OVM_CrossDomainEnabled.sol";
-import "hardhat/console.sol";
 
 contract L2_NovaRegistry is ReentrancyGuard, OVM_CrossDomainEnabled {
     using OVM_SafeERC20 for IERC20;
