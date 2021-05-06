4ea8600abfbfee1fc9f444b092189b47232d4cf4
t11s <greenfeatherhelp@gmail.com>
2021-05-05 21:42:34 -0700

    ♻️ Use UniswapV2ERC20 for Mock OVM ETH

--- contracts/mocks/MockOVMETH.sol
+++ contracts/mocks/MockOVMETH.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.7.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@eth-optimism/contracts/libraries/standards/UniswapV2ERC20.sol";
 
-contract MockOVMETH is ERC20 {
-    constructor() ERC20("Ethereum", "ETH") {
+contract MockOVMETH is UniswapV2ERC20 {
+    constructor() UniswapV2ERC20("Ethereum", "ETH") {
         // 10k premint
-        _mint(msg.sender, 10_000 * 10**decimals());
+        _mint(msg.sender, 10_000 * 10**decimals);
     }
 }
