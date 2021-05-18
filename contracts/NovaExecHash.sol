a5caa65f396dc930a5b6f2726e0a64da34f63a07
t11s <greenfeatherhelp@gmail.com>
2021-05-18 10:08:57 -0700

    ♻️ Make NovaExecHash an abstract

--- contracts/NovaExecHash.sol
+++ contracts/NovaExecHash.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.7.6;
 
-/// @notice Utility library to compute an execHash in Nova format from a nonce, strategy address, calldata and gas price.
-library NovaExecHash {
+/// @notice Utility base to compute a Nova execHash from a nonce, strategy address, calldata and gas price.
+abstract contract NovaExecHash {
     /// @notice Computes a Nova execHash from a nonce, strategy address, calldata and gas price.
     /// @return A Nova execHash.
-    function compute(
+    function computeNovaExecHash(
         uint256 nonce,
         address strategy,
         bytes memory l1calldata,
         uint256 gasPrice
-    ) external pure returns (bytes32) {
+    ) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(nonce, strategy, l1calldata, gasPrice));
     }
 }
